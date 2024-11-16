8dbf1d8c5dfd113353cf21c602857311b099d74d
woodyjacques <woodyjacques1@gmail.com>
2024-11-15 21:57:17 -0400

    Cambios

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,17 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../view/Home";
-import Register from "../view/Register";
-import Login from "../view/Login";
-import VerEmail from "../view/VerEmail";
-import UpEmail from "../view/UpEmail";
 
 
 const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    // { path: "/register", element: <Register /> },
-    // { path: "/login", element: <Login /> },
-    // { path: "/verificacion-cuenta", element: <VerEmail /> },
-    // { path: "/actualizacion-contraseña", element: <UpEmail /> }
+    { path: "/", element: <Home /> }
 ]);
 
 export default router;
